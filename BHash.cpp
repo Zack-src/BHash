34746246eff00b6b289a15e10539ae6047a60383
Zack-src <57723444+Zack-src@users.noreply.github.com>
2021-01-11 15:44:56 +0100

    Update BHash.cpp

--- BHash.cpp
+++ BHash.cpp
@@ -3,8 +3,8 @@
 int Tableau(int Text, int pos) {
 	int table[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-	for (int i = 0; i < Text; i++) {
-		for (int j = 0; j < 10; j++) {
+	for (int i = 0; i < Text; i++)
+		for (int j = 0; j < 10; j++)
 			table[j] = table[j] + (i * (pos + 4));
 			
 	return table[Text % 10];
